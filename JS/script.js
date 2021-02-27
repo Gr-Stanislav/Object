e2c0ec60c7e3a14abf572b722d157ca31b065214
Gr-Stanislav <grstas1990@gmail.com>
2021-02-27 16:57:33 +0200

    auto

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+let auto = {
+    brand: " BMW ",
+    model: " X6 ",
+    year: 2020,
+    speed: 250,
+}
+
+
+if (confirm ("Показать информацию по выбранному авто?")) {
+    alert (auto.brand + auto.model + "Year: - " + auto.year + " Max.speed: - " + auto.speed);
+
+    let average_speed = parseInt(prompt("Введите расстояние которое хотели бы приодалеть на данном авто? Введите расстояние в километрах:"));
+    let time = average_speed / (auto.speed / 2);
+    console.log (time + " Часа в пути.");
+
+    if (time >= 4) {
+    let relaxation;
+    relaxation = time / 4;
+    alert (`Время в пути: ${time} часа,` + ` + ~ время отдыха в пути ${relaxation} часа.`);
+    } else {
+        alert (time + " часа в пути. Маршрут можно проехать без отдыха.");
+    }
+
+} else {
+    alert ("Ок, в другой раз!")
+}
+
+
+
+
